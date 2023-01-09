ff71e033e2f4d863ec50ba0be6d0bc3d5f3b0bb3
Bryton Hall <email@bryton.io>
2023-01-08 22:40:01 -0500

    add github workflow to build and publish

--- flake.nix
+++ flake.nix
@@ -5,25 +5,31 @@
   };
 
   outputs = inputs@{ self, ... }:
-    inputs.utils.lib.eachDefaultSystem (system:
-      let pkgs = inputs.nixpkgs.legacyPackages.${system}; in
-      {
-        packages.default = pkgs.stdenv.mkDerivation {
-          name = "little-buddy";
-          src = ./.;
-          makeFlags = [
-            # "-j"
-            "T=open_source"
-          ];
-          nativeBuildInputs = with pkgs; [
-            # https://github.com/NixOS/nixpkgs/issues/51907
-            gcc-arm-embedded-9
+    with inputs.utils.lib; eachSystem
+      (with system; [
+        x86_64-linux
+        aarch64-linux
+        # `gcc-arm-embedded-9` not supported on `aarch64-darwin`
+      ])
+      (system:
+        let pkgs = inputs.nixpkgs.legacyPackages.${system}; in
+        {
+          packages.default = pkgs.stdenv.mkDerivation {
+            name = "little-buddy";
+            src = ./.;
+            makeFlags = [
+              # "-j"
+              "T=open_source"
+            ];
+            nativeBuildInputs = with pkgs; [
+              # https://github.com/NixOS/nixpkgs/issues/51907
+              gcc-arm-embedded-9
 
-            bc
-            hostname
-            flashrom
-            minicom
-          ];
-        };
-      });
+              bc
+              hostname
+              flashrom
+              minicom
+            ];
+          };
+        });
 }
